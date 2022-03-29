33faf28436d06de46c8751418ddcdaee1b1332c8
Hari Dhungel <haridhungel@Haris-MacBook-Pro.local>
2022-03-29 10:01:56 -0400

    front end added

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ require('dotenv').config()
 const getAllUsers = require('./src/routes/users/getAllUsers')
 const createUser = require('./src/routes/users/createUser');
 const updateUser = require('./src/routes/users/updateUser');
+const loginUser = require('./src/routes/users/loginUser');
+const logoutUser = require('./src/routes/users/logoutUser');
+
 const getAllLists = require('./src/routes/lists/getAllLists');
 const createList = require('./src/routes/lists/createList');
 const updateList = require('./src/routes/lists/updateList');
@@ -13,6 +16,8 @@ const app = express();
 app.get('/api/v1/users/', getAllUsers)
 app.post('/api/v1/users', createUser);
 app.put('/api/v1/users', updateUser);
+app.post('/api/v1/users', loginUser);
+app.post('/api/v1/users', logoutUser);
 
 app.get('/api/v1/list/', getAllLists)
 app.post('/api/v1/list', createList);
