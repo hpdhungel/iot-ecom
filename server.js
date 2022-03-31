5658cede63774f454135fd2e29037659baf3a8c3
Hari Dhungel <haridhungel@Haris-MacBook-Pro.local>
2022-03-31 13:45:23 -0400

    added css prime ng and started connecting backend vai service

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const getAllUsers = require('./src/routes/users/getAllUsers')
 const createUser = require('./src/routes/users/createUser');
 const updateUser = require('./src/routes/users/updateUser');
 const loginUser = require('./src/routes/users/loginUser');
-const logoutUser = require('./src/routes/users/logoutUser');
+// const logoutUser = require('./src/routes/users/logoutUser');
 
 const getAllLists = require('./src/routes/lists/getAllLists');
 const createList = require('./src/routes/lists/createList');
@@ -17,7 +17,7 @@ app.get('/api/v1/users/', getAllUsers)
 app.post('/api/v1/users', createUser);
 app.put('/api/v1/users', updateUser);
 app.post('/api/v1/users', loginUser);
-app.post('/api/v1/users', logoutUser);
+// app.post('/api/v1/users', logoutUser);
 
 app.get('/api/v1/list/', getAllLists)
 app.post('/api/v1/list', createList);
