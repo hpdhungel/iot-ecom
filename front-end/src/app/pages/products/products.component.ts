2433bc1c222dafb4fb51901b9a0790dd04967324
Hari Dhungel <haridhungel@Haris-MacBook-Pro.local>
2022-04-08 11:22:57 -0400

    added update and cart backennd function

--- front-end/src/app/pages/products/products.component.ts
+++ front-end/src/app/pages/products/products.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { ProductService } from './services/product.service';
+import { Message } from 'primeng/api';
+import { ConfirmationService } from 'primeng/api';
 
 @Component({
   selector: 'app-products',
@@ -8,57 +10,105 @@ import { ProductService } from './services/product.service';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-  products: any
+  constructor(private confirmationService: ConfirmationService, private productService: ProductService) { }
 
-  constructor(private productService: ProductService) {  }
   name = new FormControl("");
   description = new FormControl("");
   price = new FormControl("")
   quantity = new FormControl("")
   id = new FormControl("")
 
-  showProductForm:boolean = false;
+  products: any
+
+  showProductForm: boolean = false;
+  createProductModel: boolean;
+  msgs: Message[] = [];
+  deleteStatus: any
+  messageTitle: any
+  message: any
 
   ngOnInit() {
-    this.getProduct()
+    this.getAllProduct()
   }
 
-  getProduct(){
-    this.productService.getProducts().subscribe(data => {
+  wait(sec) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve('resolved');
+      }, sec);
+    });
+  }
+
+  showForm() {
+    this.showProductForm = !this.showProductForm
+  }
+
+  messageModel(title, mess) {
+    this.createProductModel = true;
+
+    this.message = mess
+    this.messageTitle = title
+  }
+
+  getAllProduct() {
+    this.productService.getProducts().subscribe((data) => {
       this.products = data
-      console.log(this.products)
+    },
+    (error) => {
+      this.messageModel(error.name, error.statusText)
     })
   }
 
-  async addProduct() {
-    this.productService.createProduct(this.name.value, this.description.value, this.price.value, this.quantity.value ).subscribe(data => {
-        console.log(data)
-      
-        this.showProductForm = false
-      
-    
-    })
-    this.getProduct()
-
-  }
-
-  updateProduct() {
-    this.productService.updateProduct(this.name.value, this.description.value, this.price.value, this.quantity.value, this.id.value).subscribe(data => {
-     if (data>0){
-      this.addProduct()  
-    }    
+  addProduct() {
+    this.productService.createProduct(this.name.value, this.description.value, this.price.value, this.quantity.value).subscribe(() => {
+      this.wait(1)
+      this.messageModel("success", "success")
+      this.ngOnInit()
       this.showProductForm = false
+    },
+    (error) => {
+      this.wait(1)
+      this.messageModel(error.name, error.statusText)
     })
   }
 
   deleteProduct(id) {
-    this.productService.deleteProduct(id).subscribe((data) => {
-     this.getProduct(),
-     console.log(data)
+    this.confirmationService.confirm({
+      message: 'Are you sure that you want to proceed?',
+      header: 'Confirmation',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.productService.deleteProduct(id).subscribe(
+          (data) => {
+            this.deleteStatus = data
+            if (this.deleteStatus.message === 'Deleted') {
+              this.wait(1)
+              this.ngOnInit()
+              this.msgs = [{ severity: 'info', summary: 'Confirmed', detail: 'You have successfully deleted' }];
+            }
+          },
+
+          (error) => {
+            this.wait(1)
+            this.messageModel(error.name, error.statusText)
+          })
+      },
+      reject: () => {
+        this.msgs = [{ severity: 'info', summary: 'Rejected', detail: 'You have cancled' }];
+      },
+    });
+  }
+
+  updateProduct(product) {
+    console.log(product)
+    this.productService.updateProduct(this.name.value, this.description.value, this.price.value, this.quantity.value, this.id.value).subscribe(data => {
+      if (data > 0) {
+        this.addProduct(),
+          this.showProductForm = false
+      } else {
+        console.log('something went wrong')
+      }
     })
   }
-  
-  showForm(){
-    this.showProductForm = !this.showProductForm
-  }
+
 }
